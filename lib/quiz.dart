49f45f37af0052050124d37c9936c90178ace332
Marawan Ashraf Abdelghany <marawan@192.168.1.12>
2024-11-26 14:32:14 +0200

    Initial Commit

---
+++ lib/quiz.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _Quiz();
+  }
+}
+
+class _Quiz extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+  void switchScreen() {
+    activeScreen = const QuestionsScreen();
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 78, 13, 151),
+                  Color.fromARGB(255, 107, 15, 168),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: activeScreen),
+      ),
+    );
+  }
+}
